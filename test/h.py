c0a54695736b03ac7ebdbe1d502937dd1394aa04
Ivan <109616511+MrBrain-YT@users.noreply.github.com>
2024-01-02 13:09:00 +0300

    Templates update

--- test/h.py
+++ test/h.py
@@ -1,14 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 def test(app:Flask):
     
-    @app.route('/test', methods = ['GET'])
+    @app.route('/', methods = ['GET'])
     def test_site():
-        return "hello world"
+        return render_template("help/h.html")
     
     
 if __name__ == "__main__":
     app = Flask(__name__)
     test(app)
-    app.run()
