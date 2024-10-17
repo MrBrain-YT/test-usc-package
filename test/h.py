2a0b5e0511bb309a35541a98b22e2c7c2ba97140
MrBrain-YT <109616511+MrBrain-YT@users.noreply.github.com>
2024-10-17 20:46:35 +0300

    Package update
    Update for USC v2.0.0

--- test/h.py
+++ test/h.py
@@ -1,13 +1,8 @@
 from flask import Flask, render_template
 
-
 def main(app:Flask):
     
-    @app.route('/test/', methods = ['GET'])
-    def test_site():
+    @app.route('/test/', methods = ['GET'], endpoint='test_home_page')
+    def home_page():
         return render_template("test/test.html")
     
-    
-if __name__ == "__main__":
-    app = Flask(__name__)
-    test(app)
